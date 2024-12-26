41eaef6cf9b07884629c99386c647e53fc2693dc
Aryan Shahi <rnshahi34@gmail.com>
2024-12-26 12:04:44 +0545

    Confusing C Problems

---
+++ 03bitter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    int a=9,b=0,c=0;
+    if( !(a<10) && !b || c)
+    printf("difficulties make us better");
+    else
+    printf("difficulties make us bitter");
+}
---
+++ 05inverse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    int a=0,b=0;
+    if(!a){
+        b=!a;
+        if(b)
+        a=!b;
+    }
+    printf("%d %d \n",a,b);
+}
